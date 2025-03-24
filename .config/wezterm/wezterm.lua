85c9acc0cde8e2bd055cdf6c94130f9b4738697a
Andre Gaudencio <andre.gaudencio@one.network>
2025-03-24 11:40:43 +0000

    Add alacritty, zellij and update wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,13 +2,64 @@ local wezterm = require("wezterm")
 
 local config = {
   color_scheme = 'tokyonight_night',
-  font = wezterm.font('JetBrains Mono'),
-  font_size = 13.0,
+  --font = wezterm.font('JetBrainsMono Nerd Font'),
+  --font = wezterm.font('MesloLGS Nerd Font Mono'),
+  font = wezterm.font_with_fallback({
+    --"JetBrainsMono Nerd Font", -- <built-in>, BuiltIn
+    "MesloLGS Nerd Font Mono",
+  }, { weight = "Bold", italic = false }),
+
+  font_rules= {
+    {
+      italic = true,
+      font = wezterm.font("MesloLGS Nerd Font Mono", {italic=true}),
+    },
+
+    {
+      italic = true,
+      intensity = "Bold",
+      font = wezterm.font("MesloLGS Nerd Font Mono", {bold=true,italic=true}),
+    },
+
+    {
+      intensity = "Bold",
+      font = wezterm.font("MesloLGS Nerd Font Mono", {bold=true}),
+    },
+  },
+
+  foreground_text_hsb = {
+    hue = 1.0,
+    saturation = 1.0,
+    brightness = 0.8,  -- default is 1.0
+  },
+
   audible_bell = 'Disabled',
   pane_focus_follows_mouse = true,
   scrollback_lines = 100000,
+  font_size = 13.0,
+  line_height = 1.111,
+  --front_end = 'OpenGL',
+
+  --freetype_load_target = 'Light',
+  freetype_load_target = 'HorizontalLcd',
+
+  --freetype_render_target = 'HorizontalLcd',
+
+  cell_width = 0.9,
+
   -- integrate the tabs into the window title bar
   window_decorations = "INTEGRATED_BUTTONS|RESIZE",
+
+  --custom_block_glyphs = true,
+
+  freetype_load_flags = 'NO_HINTING|NO_AUTOHINT',
+  --freetype_load_flags = 'NO_HINTING|NO_BITMAP',
+
+  front_end = "WebGpu",
+  webgpu_power_preference = "HighPerformance",
+  bold_brightens_ansi_colors = true,
+
+  hide_tab_bar_if_only_one_tab = true,
 
   keys = {
     {
@@ -78,18 +129,3 @@ local config = {
 }
 
 return config
-
----- WezTerm Configuration
---local wezterm = require 'wezterm'
---local config = {}
---
---if wezterm.config_builder then
---  config = wezterm.config_builder()
---end
---
---config.color_scheme = 'Catppuccin Mocha'
---config.font = wezterm.font('JetBrainsMono Nerd Font')
---config.font_size = 13.0
---config.audible_bell = 'Disabled'
---
---return config
