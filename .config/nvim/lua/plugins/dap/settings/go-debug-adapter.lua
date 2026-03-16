a0457f5826c3648b3c0ccdfc64242516946695fe
Andre Gaudencio <andre.gaudencio@one.network>
2026-03-16 15:09:57 +0000

    Add golang test debug configs

--- .config/nvim/lua/plugins/dap/settings/go-debug-adapter.lua
+++ .config/nvim/lua/plugins/dap/settings/go-debug-adapter.lua
@@ -15,6 +15,22 @@ dap.configurations.go = {
     request = "launch",
     showLog = false,
     program = "${file}",
-    dlvToolPath = vim.fn.stdpath("data") .. "/mason/bin/dlv", -- optional but recommended
+    dlvToolPath = vim.fn.stdpath("data") .. "/mason/bin/dlv",
+  },
+  {
+    type = "go",
+    name = "Debug Test",
+    request = "launch",
+    mode = "test",
+    cwd = function()
+      return vim.fn.getcwd()
+    end,
+    program = function()
+      local file_dir = vim.fn.expand("%:p:h")
+      local cwd = vim.fn.getcwd()
+      local rel = file_dir:gsub("^" .. vim.pesc(cwd) .. "/", "")
+      return "./" .. rel
+    end,
+    dlvToolPath = vim.fn.stdpath("data") .. "/mason/bin/dlv",
   },
 }
